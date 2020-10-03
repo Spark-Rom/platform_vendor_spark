2bd5cdee97bf6cebf18b0925132d00658acfb2b4
jhenrique09 <jhenrique09@users.noreply.github.com>
2020-10-03 21:14:47 +0000

    vendor: Allow providing camera server and service
    Change-Id: I6d56930f4673cf7bdf99fccb87c15f0c0fc9a1d7

--- config/BoardConfigSpark.mk
+++ config/BoardConfigSpark.mk
@@ -1,2 +1,8 @@
 include vendor/spark/config/BoardConfigKernel.mk
 include vendor/spark/config/BoardConfigSoong.mk
+
+ifneq ($(TARGET_USES_PREBUILT_CAMERA_SERVICE), true)
+PRODUCT_SOONG_NAMESPACES += \
+    frameworks/av/camera/cameraserver \
+    frameworks/av/services/camera/libcameraservice
+endif
