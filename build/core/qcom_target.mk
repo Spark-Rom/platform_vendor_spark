dasshubham762/vendor_derp

vendor: Import build additions from POSP
Change-Id: I83ceb30c3d03212876a328c57bcf038e998fa007
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

build/core/qcom_target.mk
@@ -1,0 +1,30 @@
+# Bring in Qualcomm helper macros
+include vendor/derp/build/core/qcom_utils.mk
+
+# Set device-specific HALs into project pathmap
+define set-device-specific-path
+$(if $(USE_DEVICE_SPECIFIC_$(1)), \
+    $(if $(DEVICE_SPECIFIC_$(1)_PATH), \
+        $(eval path := $(DEVICE_SPECIFIC_$(1)_PATH)), \
+        $(eval path := $(TARGET_DEVICE_DIR)/$(2))), \
+    $(eval path := $(3))) \
+$(call project-set-path,qcom-$(2),$(strip $(path)))
+endef
+
+ifeq ($(PRODUCT_USES_QCOM_HARDWARE),true)
+
+$(call set-device-specific-path,AUDIO,audio,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/audio)
+$(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/display)
+$(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/media)
+
+$(call set-device-specific-path,BT_VENDOR,bt-vendor,hardware/qcom-caf/bt)
+$(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,vendor/qcom/opensource/data-ipa-cfg-mgr)
+$(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
+$(call set-device-specific-path,THERMAL,thermal,hardware/qcom-caf/thermal)
+$(call set-device-specific-path,VR,vr,hardware/qcom-caf/vr)
+$(call set-device-specific-path,WLAN,wlan,hardware/qcom-caf/wlan)
+
+PRODUCT_CFI_INCLUDE_PATHS += \
+    hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
+
+endif

build/soong/generator/generator.go
@@ -1,0 +1,309 @@
+// Copyright 2015 Google Inc. All rights reserved.
+// Copyright (C) 2018 The LineageOS Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package generator
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/google/blueprint"
+	"github.com/google/blueprint/bootstrap"
+	"github.com/google/blueprint/proptools"
+
+	"android/soong/android"
+	"android/soong/shared"
+	"path/filepath"
+)
+
+func init() {
+	android.RegisterModuleType("derp_generator", GeneratorFactory)
+
+	pctx.HostBinToolVariable("sboxCmd", "sbox")
+}
+
+var String = proptools.String
+
+var (
+	pctx = android.NewPackageContext("android/soong/generator")
+)
+
+type HostToolProvider interface {
+	HostToolPath() android.OptionalPath
+}
+
+type hostToolDependencyTag struct {
+	blueprint.BaseDependencyTag
+}
+
+var hostToolDepTag hostToolDependencyTag
+
+type generatorProperties struct {
+	// The command to run on one or more input files. Cmd supports substitution of a few variables
+	// (the actual substitution is implemented in GenerateAndroidBuildActions below)
+	//
+	// Available variables for substitution:
+	//
+	//  $(location): the path to the first entry in tools or tool_files
+	//  $(location <label>): the path to the tool or tool_file with name <label>
+	//  $(genDir): the sandbox directory for this tool; contains $(out)
+	//  $$: a literal $
+	//
+	Cmd *string
+
+	// name of the modules (if any) that produces the host executable.   Leave empty for
+	// prebuilts or scripts that do not need a module to build them.
+	Tools []string
+
+	// Local file that is used as the tool
+	Tool_files []string
+
+	// List of directories to export as headers
+	Export_include_dirs []string
+
+	// List of directories to export as sources
+	Export_source_dirs []string
+
+	// Root directory for dep_files.
+	// Relative to top build dir.
+	Dep_root *string
+
+	// Declare list of files that should be used for timestamp dependency checking
+	Dep_files []string
+}
+
+type Module struct {
+	android.ModuleBase
+
+	properties generatorProperties
+
+	rule blueprint.Rule
+
+	// Tool dependencies
+	implicitDeps android.Paths
+	// Deps from input files declared in dep_files property.
+	inputDeps android.Paths
+
+	exportedIncludeDirs android.Paths
+	exportedSourceDirs  android.Paths
+
+	outputDeps android.Paths
+}
+
+// These three methods satisfy genrule.SourceFileGenerator.
+// Which cc modules check for when including headers etc.
+func (g *Module) GeneratedHeaderDirs() android.Paths {
+	return g.exportedIncludeDirs
+}
+
+func (g *Module) GeneratedSourceFiles() android.Paths {
+	return g.exportedSourceDirs
+}
+
+func (g *Module) GeneratedDeps() android.Paths {
+	return g.outputDeps
+}
+
+func (g *Module) DepsMutator(ctx android.BottomUpMutatorContext) {
+	android.ExtractSourcesDeps(ctx, g.properties.Dep_files)
+	android.ExtractSourcesDeps(ctx, g.properties.Tool_files)
+	if g, ok := ctx.Module().(*Module); ok {
+		if len(g.properties.Tools) > 0 {
+			ctx.AddFarVariationDependencies(ctx.Config().BuildOSTarget.Variations(),
+			hostToolDepTag, g.properties.Tools...)
+		}
+	}
+}
+
+func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
+	if len(g.properties.Export_include_dirs) > 0 {
+		for _, dir := range g.properties.Export_include_dirs {
+			g.exportedIncludeDirs = append(g.exportedIncludeDirs,
+				android.PathForModuleGen(ctx, dir))
+		}
+	} else {
+		g.exportedIncludeDirs = append(g.exportedIncludeDirs, android.PathForModuleGen(ctx, ""))
+	}
+	if len(g.properties.Export_source_dirs) > 0 {
+		for _, dir := range g.properties.Export_source_dirs {
+			g.exportedSourceDirs = append(g.exportedSourceDirs,
+				android.PathForModuleGen(ctx, dir))
+		}
+	} else {
+		g.exportedSourceDirs = append(g.exportedSourceDirs, android.PathForModuleGen(ctx, ""))
+	}
+
+	tools := map[string]android.Path{}
+
+	if len(g.properties.Tools) > 0 {
+		ctx.VisitDirectDepsBlueprint(func(module blueprint.Module) {
+			switch ctx.OtherModuleDependencyTag(module) {
+			case android.SourceDepTag:
+				// Nothing to do
+			case hostToolDepTag:
+				tool := ctx.OtherModuleName(module)
+				var path android.OptionalPath
+
+				if t, ok := module.(HostToolProvider); ok {
+					if !t.(android.Module).Enabled() {
+						if ctx.Config().AllowMissingDependencies() {
+							ctx.AddMissingDependencies([]string{tool})
+						} else {
+							ctx.ModuleErrorf("depends on disabled module %q", tool)
+						}
+						break
+					}
+					path = t.HostToolPath()
+				} else if t, ok := module.(bootstrap.GoBinaryTool); ok {
+					if s, err := filepath.Rel(android.PathForOutput(ctx).String(), t.InstallPath()); err == nil {
+						path = android.OptionalPathForPath(android.PathForOutput(ctx, s))
+					} else {
+						ctx.ModuleErrorf("cannot find path for %q: %v", tool, err)
+						break
+					}
+				} else {
+					ctx.ModuleErrorf("%q is not a host tool provider", tool)
+					break
+				}
+
+				if path.Valid() {
+					g.implicitDeps = append(g.implicitDeps, path.Path())
+					if _, exists := tools[tool]; !exists {
+						tools[tool] = path.Path()
+					} else {
+						ctx.ModuleErrorf("multiple tools for %q, %q and %q", tool, tools[tool], path.Path().String())
+					}
+				} else {
+					ctx.ModuleErrorf("host tool %q missing output file", tool)
+				}
+			default:
+				ctx.ModuleErrorf("unknown dependency on %q", ctx.OtherModuleName(module))
+			}
+		})
+	}
+
+	if ctx.Failed() {
+		return
+	}
+
+	toolFiles := ctx.ExpandSources(g.properties.Tool_files, nil)
+	for _, tool := range toolFiles {
+		g.implicitDeps = append(g.implicitDeps, tool)
+		if _, exists := tools[tool.Rel()]; !exists {
+			tools[tool.Rel()] = tool
+		} else {
+			ctx.ModuleErrorf("multiple tools for %q, %q and %q", tool, tools[tool.Rel()], tool.Rel())
+		}
+	}
+
+	// Determine root dir for dep_files.  Defaults to current ctx ModuleDir.
+	depRoot := String(g.properties.Dep_root)
+	if depRoot == "" {
+		depRoot = ctx.ModuleDir()
+	} else {
+		depRoot = derpExpandVariables(ctx, depRoot)
+	}
+
+	// Glob dep_files property
+	for _, dep_file := range g.properties.Dep_files {
+		dep_file = derpExpandVariables(ctx, dep_file)
+		globPath := filepath.Join(depRoot, dep_file)
+		paths, err := ctx.GlobWithDeps(globPath, nil)
+		if err != nil {
+			ctx.ModuleErrorf("unable to glob %s: %s", globPath, err.Error())
+			return
+		}
+		for _, path := range paths {
+			g.inputDeps = append(g.inputDeps, android.PathForSourceRelaxed(ctx, path))
+		}
+	}
+
+	cmd := derpExpandVariables(ctx, String(g.properties.Cmd))
+
+	rawCommand, err := android.Expand(cmd, func(name string) (string, error) {
+		switch name {
+		case "location":
+			if len(g.properties.Tools) == 0 && len(toolFiles) == 0 {
+				return "", fmt.Errorf("at least one `tools` or `tool_files` is required if $(location) is used")
+			}
+
+			if len(g.properties.Tools) > 0 {
+				return tools[g.properties.Tools[0]].String(), nil
+			} else {
+				return tools[toolFiles[0].Rel()].String(), nil
+			}
+		case "genDir":
+			return "__SBOX_OUT_DIR__", nil
+		default:
+			if strings.HasPrefix(name, "location ") {
+				label := strings.TrimSpace(strings.TrimPrefix(name, "location "))
+				if tool, ok := tools[label]; ok {
+					return tool.String(), nil
+				} else {
+					return "", fmt.Errorf("unknown location label %q", label)
+				}
+			}
+			return "", fmt.Errorf("unknown variable '$(%s)'", name)
+		}
+	})
+
+	if err != nil {
+		ctx.PropertyErrorf("cmd", "%s", err.Error())
+		return
+	}
+
+	// Dummy output dep
+	dummyDep := android.PathForModuleGen(ctx, ".dummy_dep")
+
+	// tell the sbox command which directory to use as its sandbox root
+	buildDir := android.PathForOutput(ctx).String()
+	sandboxPath := shared.TempDirForOutDir(buildDir)
+
+	genDir := android.PathForModuleGen(ctx)
+	// Escape the command for the shell
+	rawCommand = "'" + strings.Replace(rawCommand, "'", `'\''`, -1) + "'"
+	sandboxCommand := fmt.Sprintf("$sboxCmd --sandbox-path %s --output-root %s --copy-all-output -c %s && touch %s",
+		sandboxPath, genDir, rawCommand, dummyDep.String())
+
+	ruleParams := blueprint.RuleParams{
+		Command:     sandboxCommand,
+		CommandDeps: []string{"$sboxCmd"},
+	}
+	g.rule = ctx.Rule(pctx, "generator", ruleParams)
+
+	params := android.BuildParams{
+		Rule:        g.rule,
+		Description: "generate",
+		Output:      dummyDep,
+		Inputs:      g.inputDeps,
+		Implicits:   g.implicitDeps,
+	}
+
+	g.outputDeps = append(g.outputDeps, dummyDep)
+
+	ctx.Build(pctx, params)
+}
+
+func NewGenerator() *Module {
+	module := &Module{}
+	module.AddProperties(&module.properties)
+	return module
+}
+
+func GeneratorFactory() android.Module {
+	m := NewGenerator()
+	android.InitAndroidModule(m)
+	return m
+}

build/soong/generator/variables.go
@@ -1,0 +1,28 @@
+package generator
+
+import (
+	"fmt"
+
+	"android/soong/android"
+)
+
+func derpExpandVariables(ctx android.ModuleContext, in string) string {
+	derpVars := ctx.Config().VendorConfig("derpVarsPlugin")
+
+	out, err := android.Expand(in, func(name string) (string, error) {
+		if derpVars.IsSet(name) {
+			return derpVars.String(name), nil
+		}
+		// This variable is not for us, restore what the original
+		// variable string will have looked like for an Expand
+		// that comes later.
+		return fmt.Sprintf("$(%s)", name), nil
+	})
+
+	if err != nil {
+		ctx.PropertyErrorf("%s: %s", in, err.Error())
+		return ""
+	}
+
+	return out
+}

build/soong/soong_config.mk
@@ -1,0 +1,29 @@
+add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
+add_json_val_default = $(call add_json_val, $(1), $(if $(strip $(2)), $(2), $(3)))
+
+_json_contents := $(_json_contents)    "Derp":{$(newline)
+
+$(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+$(call add_json_val_default, Bootloader_message_offset, $(BOOTLOADER_MESSAGE_OFFSET), 0)
+$(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
+$(call add_json_bool, Device_support_hwfde, $(filter true,$(TARGET_HW_DISK_ENCRYPTION)))
+$(call add_json_bool, Device_support_hwfde_perf, $(filter true,$(TARGET_HW_DISK_ENCRYPTION_PERF)))
+$(call add_json_bool, Device_support_legacy_hwfde, $(filter true,$(TARGET_LEGACY_HW_DISK_ENCRYPTION)))
+$(call add_json_bool, Device_support_wait_for_qsee, $(filter true,$(TARGET_KEYMASTER_WAIT_FOR_QSEE)))
+$(call add_json_str, Java_Source_Overlays, $(JAVA_SOURCE_OVERLAYS))
+$(call add_json_bool, Needs_text_relocations, $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
+$(call add_json_str_omitempty, Qcom_um_soong_namespace, $(if $(filter $(UM_PLATFORMS),$(PRODUCT_BOARD_PLATFORM)),$(QCOM_SOONG_NAMESPACE),))
+$(call add_json_str, Specific_camera_parameter_library, $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
+$(call add_json_str_omitempty, Target_init_vendor_lib, $(TARGET_INIT_VENDOR_LIB))
+$(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
+$(call add_json_str, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
+$(call add_json_bool, Target_needs_netd_direct_connect_rule, $(filter true,$(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)))
+$(call add_json_str_omitempty, Target_surfaceflinger_fod_lib, $(TARGET_SURFACEFLINGER_FOD_LIB))
+$(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true))
+$(call add_json_bool, Target_ignores_ftp_pptp_conntrack_failure, $(filter true,$(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)))
+$(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
+
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_json_contents := $(_json_contents)__SV_END
+
+_json_contents := $(_json_contents)    },$(newline)

build/tools/buildinfo.sh
@@ -1,0 +1,15 @@
+#!/bin/bash
+
+echo ""
+echo "# begin derp build properties"
+echo "# autogenerated by buildinfo.sh (derp)"
+
+GET_VERSION="vendor/derp/build/tools/getversion.py"
+echo "ro.build.project=derp"
+echo "ro.derp.branding.version=$($GET_VERSION buildtype)"
+echo "ro.derp.buildtype=$($GET_VERSION buildtype)"
+echo "ro.derp.status=$($GET_VERSION status)"
+echo "ro.derp.version=$($GET_VERSION version)"
+echo "ro.build.datetime=`$DATE +%s`"
+
+echo "# end derp build properties"

Spark-Rom/platform_vendor_spark

Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>

build/core/qcom_target.mk
@@ -1,0 +1,31 @@
+# Bring in Qualcomm helper macros
+include vendor/spark/build/core/qcom_utils.mk
+
+# Set device-specific HALs into project pathmap
+define set-device-specific-path
+$(if $(USE_DEVICE_SPECIFIC_$(1)), \
+    $(if $(DEVICE_SPECIFIC_$(1)_PATH), \
+        $(eval path := $(DEVICE_SPECIFIC_$(1)_PATH)), \
+        $(eval path := $(TARGET_DEVICE_DIR)/$(2))), \
+    $(eval path := $(3))) \
+$(call project-set-path,qcom-$(2),$(strip $(path)))
+endef
+
+ifeq ($(PRODUCT_USES_QCOM_HARDWARE),true)
+
+$(call set-device-specific-path,AUDIO,audio,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/audio)
+$(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/display)
+$(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/media)
+
+$(call set-device-specific-path,BT_VENDOR,bt-vendor,hardware/qcom-caf/bt)
+$(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,vendor/qcom/opensource/data-ipa-cfg-mgr)
+$(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
+$(call set-device-specific-path,POWER,power,hardware/qcom-caf/power)
+$(call set-device-specific-path,THERMAL,thermal,hardware/qcom-caf/thermal)
+$(call set-device-specific-path,VR,vr,hardware/qcom-caf/vr)
+$(call set-device-specific-path,WLAN,wlan,hardware/qcom-caf/wlan)
+
+PRODUCT_CFI_INCLUDE_PATHS += \
+    hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
+
+endif

build/soong/generator/generator.go
@@ -1,0 +1,309 @@
+// Copyright 2015 Google Inc. All rights reserved.
+// Copyright (C) 2018 The LineageOS Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package generator
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/google/blueprint"
+	"github.com/google/blueprint/bootstrap"
+	"github.com/google/blueprint/proptools"
+
+	"android/soong/android"
+	"android/soong/shared"
+	"path/filepath"
+)
+
+func init() {
+	android.RegisterModuleType("spark_generator", GeneratorFactory)
+
+	pctx.HostBinToolVariable("sboxCmd", "sbox")
+}
+
+var String = proptools.String
+
+var (
+	pctx = android.NewPackageContext("android/soong/generator")
+)
+
+type HostToolProvider interface {
+	HostToolPath() android.OptionalPath
+}
+
+type hostToolDependencyTag struct {
+	blueprint.BaseDependencyTag
+}
+
+var hostToolDepTag hostToolDependencyTag
+
+type generatorProperties struct {
+	// The command to run on one or more input files. Cmd supports substitution of a few variables
+	// (the actual substitution is implemented in GenerateAndroidBuildActions below)
+	//
+	// Available variables for substitution:
+	//
+	//  $(location): the path to the first entry in tools or tool_files
+	//  $(location <label>): the path to the tool or tool_file with name <label>
+	//  $(genDir): the sandbox directory for this tool; contains $(out)
+	//  $$: a literal $
+	//
+	Cmd *string
+
+	// name of the modules (if any) that produces the host executable.   Leave empty for
+	// prebuilts or scripts that do not need a module to build them.
+	Tools []string
+
+	// Local file that is used as the tool
+	Tool_files []string
+
+	// List of directories to export as headers
+	Export_include_dirs []string
+
+	// List of directories to export as sources
+	Export_source_dirs []string
+
+	// Root directory for dep_files.
+	// Relative to top build dir.
+	Dep_root *string
+
+	// Declare list of files that should be used for timestamp dependency checking
+	Dep_files []string
+}
+
+type Module struct {
+	android.ModuleBase
+
+	properties generatorProperties
+
+	rule blueprint.Rule
+
+	// Tool dependencies
+	implicitDeps android.Paths
+	// Deps from input files declared in dep_files property.
+	inputDeps android.Paths
+
+	exportedIncludeDirs android.Paths
+	exportedSourceDirs  android.Paths
+
+	outputDeps android.Paths
+}
+
+// These three methods satisfy genrule.SourceFileGenerator.
+// Which cc modules check for when including headers etc.
+func (g *Module) GeneratedHeaderDirs() android.Paths {
+	return g.exportedIncludeDirs
+}
+
+func (g *Module) GeneratedSourceFiles() android.Paths {
+	return g.exportedSourceDirs
+}
+
+func (g *Module) GeneratedDeps() android.Paths {
+	return g.outputDeps
+}
+
+func (g *Module) DepsMutator(ctx android.BottomUpMutatorContext) {
+	android.ExtractSourcesDeps(ctx, g.properties.Dep_files)
+	android.ExtractSourcesDeps(ctx, g.properties.Tool_files)
+	if g, ok := ctx.Module().(*Module); ok {
+		if len(g.properties.Tools) > 0 {
+			ctx.AddFarVariationDependencies(ctx.Config().BuildOSTarget.Variations(),
+			hostToolDepTag, g.properties.Tools...)
+		}
+	}
+}
+
+func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
+	if len(g.properties.Export_include_dirs) > 0 {
+		for _, dir := range g.properties.Export_include_dirs {
+			g.exportedIncludeDirs = append(g.exportedIncludeDirs,
+				android.PathForModuleGen(ctx, dir))
+		}
+	} else {
+		g.exportedIncludeDirs = append(g.exportedIncludeDirs, android.PathForModuleGen(ctx, ""))
+	}
+	if len(g.properties.Export_source_dirs) > 0 {
+		for _, dir := range g.properties.Export_source_dirs {
+			g.exportedSourceDirs = append(g.exportedSourceDirs,
+				android.PathForModuleGen(ctx, dir))
+		}
+	} else {
+		g.exportedSourceDirs = append(g.exportedSourceDirs, android.PathForModuleGen(ctx, ""))
+	}
+
+	tools := map[string]android.Path{}
+
+	if len(g.properties.Tools) > 0 {
+		ctx.VisitDirectDepsBlueprint(func(module blueprint.Module) {
+			switch ctx.OtherModuleDependencyTag(module) {
+			case android.SourceDepTag:
+				// Nothing to do
+			case hostToolDepTag:
+				tool := ctx.OtherModuleName(module)
+				var path android.OptionalPath
+
+				if t, ok := module.(HostToolProvider); ok {
+					if !t.(android.Module).Enabled() {
+						if ctx.Config().AllowMissingDependencies() {
+							ctx.AddMissingDependencies([]string{tool})
+						} else {
+							ctx.ModuleErrorf("depends on disabled module %q", tool)
+						}
+						break
+					}
+					path = t.HostToolPath()
+				} else if t, ok := module.(bootstrap.GoBinaryTool); ok {
+					if s, err := filepath.Rel(android.PathForOutput(ctx).String(), t.InstallPath()); err == nil {
+						path = android.OptionalPathForPath(android.PathForOutput(ctx, s))
+					} else {
+						ctx.ModuleErrorf("cannot find path for %q: %v", tool, err)
+						break
+					}
+				} else {
+					ctx.ModuleErrorf("%q is not a host tool provider", tool)
+					break
+				}
+
+				if path.Valid() {
+					g.implicitDeps = append(g.implicitDeps, path.Path())
+					if _, exists := tools[tool]; !exists {
+						tools[tool] = path.Path()
+					} else {
+						ctx.ModuleErrorf("multiple tools for %q, %q and %q", tool, tools[tool], path.Path().String())
+					}
+				} else {
+					ctx.ModuleErrorf("host tool %q missing output file", tool)
+				}
+			default:
+				ctx.ModuleErrorf("unknown dependency on %q", ctx.OtherModuleName(module))
+			}
+		})
+	}
+
+	if ctx.Failed() {
+		return
+	}
+
+	toolFiles := ctx.ExpandSources(g.properties.Tool_files, nil)
+	for _, tool := range toolFiles {
+		g.implicitDeps = append(g.implicitDeps, tool)
+		if _, exists := tools[tool.Rel()]; !exists {
+			tools[tool.Rel()] = tool
+		} else {
+			ctx.ModuleErrorf("multiple tools for %q, %q and %q", tool, tools[tool.Rel()], tool.Rel())
+		}
+	}
+
+	// Determine root dir for dep_files.  Defaults to current ctx ModuleDir.
+	depRoot := String(g.properties.Dep_root)
+	if depRoot == "" {
+		depRoot = ctx.ModuleDir()
+	} else {
+		depRoot = sparkExpandVariables(ctx, depRoot)
+	}
+
+	// Glob dep_files property
+	for _, dep_file := range g.properties.Dep_files {
+		dep_file = sparkExpandVariables(ctx, dep_file)
+		globPath := filepath.Join(depRoot, dep_file)
+		paths, err := ctx.GlobWithDeps(globPath, nil)
+		if err != nil {
+			ctx.ModuleErrorf("unable to glob %s: %s", globPath, err.Error())
+			return
+		}
+		for _, path := range paths {
+			g.inputDeps = append(g.inputDeps, android.PathForSourceRelaxed(ctx, path))
+		}
+	}
+
+	cmd := sparkExpandVariables(ctx, String(g.properties.Cmd))
+
+	rawCommand, err := android.Expand(cmd, func(name string) (string, error) {
+		switch name {
+		case "location":
+			if len(g.properties.Tools) == 0 && len(toolFiles) == 0 {
+				return "", fmt.Errorf("at least one `tools` or `tool_files` is required if $(location) is used")
+			}
+
+			if len(g.properties.Tools) > 0 {
+				return tools[g.properties.Tools[0]].String(), nil
+			} else {
+				return tools[toolFiles[0].Rel()].String(), nil
+			}
+		case "genDir":
+			return "__SBOX_OUT_DIR__", nil
+		default:
+			if strings.HasPrefix(name, "location ") {
+				label := strings.TrimSpace(strings.TrimPrefix(name, "location "))
+				if tool, ok := tools[label]; ok {
+					return tool.String(), nil
+				} else {
+					return "", fmt.Errorf("unknown location label %q", label)
+				}
+			}
+			return "", fmt.Errorf("unknown variable '$(%s)'", name)
+		}
+	})
+
+	if err != nil {
+		ctx.PropertyErrorf("cmd", "%s", err.Error())
+		return
+	}
+
+	// Dummy output dep
+	dummyDep := android.PathForModuleGen(ctx, ".dummy_dep")
+
+	// tell the sbox command which directory to use as its sandbox root
+	buildDir := android.PathForOutput(ctx).String()
+	sandboxPath := shared.TempDirForOutDir(buildDir)
+
+	genDir := android.PathForModuleGen(ctx)
+	// Escape the command for the shell
+	rawCommand = "'" + strings.Replace(rawCommand, "'", `'\''`, -1) + "'"
+	sandboxCommand := fmt.Sprintf("$sboxCmd --sandbox-path %s --output-root %s --copy-all-output -c %s && touch %s",
+		sandboxPath, genDir, rawCommand, dummyDep.String())
+
+	ruleParams := blueprint.RuleParams{
+		Command:     sandboxCommand,
+		CommandDeps: []string{"$sboxCmd"},
+	}
+	g.rule = ctx.Rule(pctx, "generator", ruleParams)
+
+	params := android.BuildParams{
+		Rule:        g.rule,
+		Description: "generate",
+		Output:      dummyDep,
+		Inputs:      g.inputDeps,
+		Implicits:   g.implicitDeps,
+	}
+
+	g.outputDeps = append(g.outputDeps, dummyDep)
+
+	ctx.Build(pctx, params)
+}
+
+func NewGenerator() *Module {
+	module := &Module{}
+	module.AddProperties(&module.properties)
+	return module
+}
+
+func GeneratorFactory() android.Module {
+	m := NewGenerator()
+	android.InitAndroidModule(m)
+	return m
+}

build/soong/generator/variables.go
@@ -1,0 +1,28 @@
+package generator
+
+import (
+	"fmt"
+
+	"android/soong/android"
+)
+
+func sparkExpandVariables(ctx android.ModuleContext, in string) string {
+	sparkVars := ctx.Config().VendorConfig("sparkVarsPlugin")
+
+	out, err := android.Expand(in, func(name string) (string, error) {
+		if sparkVars.IsSet(name) {
+			return sparkVars.String(name), nil
+		}
+		// This variable is not for us, restore what the original
+		// variable string will have looked like for an Expand
+		// that comes later.
+		return fmt.Sprintf("$(%s)", name), nil
+	})
+
+	if err != nil {
+		ctx.PropertyErrorf("%s: %s", in, err.Error())
+		return ""
+	}
+
+	return out
+}

build/soong/soong_config.mk
@@ -1,0 +1,29 @@
+add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
+add_json_val_default = $(call add_json_val, $(1), $(if $(strip $(2)), $(2), $(3)))
+
+_json_contents := $(_json_contents)    "Spark":{$(newline)
+
+$(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+$(call add_json_val_default, Bootloader_message_offset, $(BOOTLOADER_MESSAGE_OFFSET), 0)
+$(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
+$(call add_json_bool, Device_support_hwfde, $(filter true,$(TARGET_HW_DISK_ENCRYPTION)))
+$(call add_json_bool, Device_support_hwfde_perf, $(filter true,$(TARGET_HW_DISK_ENCRYPTION_PERF)))
+$(call add_json_bool, Device_support_legacy_hwfde, $(filter true,$(TARGET_LEGACY_HW_DISK_ENCRYPTION)))
+$(call add_json_bool, Device_support_wait_for_qsee, $(filter true,$(TARGET_KEYMASTER_WAIT_FOR_QSEE)))
+$(call add_json_str, Java_Source_Overlays, $(JAVA_SOURCE_OVERLAYS))
+$(call add_json_bool, Needs_text_relocations, $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
+$(call add_json_str_omitempty, Qcom_um_soong_namespace, $(if $(filter $(UM_PLATFORMS),$(PRODUCT_BOARD_PLATFORM)),$(QCOM_SOONG_NAMESPACE),))
+$(call add_json_str, Specific_camera_parameter_library, $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
+$(call add_json_str_omitempty, Target_init_vendor_lib, $(TARGET_INIT_VENDOR_LIB))
+$(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
+$(call add_json_str, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
+$(call add_json_bool, Target_needs_netd_direct_connect_rule, $(filter true,$(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)))
+$(call add_json_str_omitempty, Target_surfaceflinger_fod_lib, $(TARGET_SURFACEFLINGER_FOD_LIB))
+$(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true))
+$(call add_json_bool, Target_ignores_ftp_pptp_conntrack_failure, $(filter true,$(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)))
+$(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
+
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_json_contents := $(_json_contents)__SV_END
+
+_json_contents := $(_json_contents)    },$(newline)

build/tools/buildinfo.sh
@@ -1,0 +1,15 @@
+#!/bin/bash
+
+echo ""
+echo "# begin spark build properties"
+echo "# autogenerated by buildinfo.sh (spark)"
+
+GET_VERSION="vendor/spark/build/tools/getversion.py"
+echo "ro.build.project=spark"
+echo "ro.spark.branding.version=$($GET_VERSION buildtype)"
+echo "ro.spark.buildtype=$($GET_VERSION buildtype)"
+echo "ro.spark.status=$($GET_VERSION status)"
+echo "ro.spark.version=$($GET_VERSION version)"
+echo "ro.build.datetime=`$DATE +%s`"
+
+echo "# end spark build properties"