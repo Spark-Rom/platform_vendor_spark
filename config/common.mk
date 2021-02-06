Octavi-OS/platform_vendor_octavi

Fix Backuptools
- Just adding it to make it sure
- Picked These backuptools from awaken

config/common.mk
@@ -16,6 +16,16 @@ PRODUCT_COPY_FILES += \
     vendor/octavi/prebuilt/common/bin/backuptool.functions:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.functions \
     vendor/octavi/prebuilt/common/bin/50-base.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-base.sh \
 
+# backuptool
+PRODUCT_COPY_FILES += \
+    vendor/octavi/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/octavi/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/octavi/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh
+
+# system mount
+PRODUCT_COPY_FILES += \
+    vendor/octavi/prebuilt/common/bin/system-mount.sh:install/bin/system-mount.sh
+
 ifneq ($(AB_OTA_PARTITIONS),)
 PRODUCT_COPY_FILES += \
     vendor/octavi/prebuilt/common/bin/backuptool_ab.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.sh \

Spark-Rom/platform_vendor_spark

config/common.mk
@@ -16,6 +16,16 @@ PRODUCT_COPY_FILES += \
     vendor/spark/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/spark/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
 
+# backuptool
+PRODUCT_COPY_FILES += \
+    vendor/spark/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/spark/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/spark/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh
+
+# system mount
+PRODUCT_COPY_FILES += \
+    vendor/spark/prebuilt/common/bin/system-mount.sh:install/bin/system-mount.sh
+
 ifneq ($(AB_OTA_PARTITIONS),)
 PRODUCT_COPY_FILES += \
     vendor/spark/prebuilt/common/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \