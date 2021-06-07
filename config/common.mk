Evolution-X-AOSP/vendor_evolution

vendor: Allow Settings privapp permission to FONT_MANAGER
Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
Change-Id: I7cbf1f33f83b31858963b258aaa63e43b4f415e6
Signed-off-by: Jackeagle <jackeagle102@gmail.com>

config/common.mk
@@ -76,6 +76,10 @@ PRODUCT_COPY_FILES += \
     vendor/evolution/config/permissions/backup.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/backup.xml \
     vendor/evolution/config/permissions/privapp-permissions-evolution.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-evolution.xml
 
+# Font service Permissions
+PRODUCT_COPY_FILES += \
+    vendor/evolution/config/permissions/privapp-permissions-evolution-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-evolution-system_ext.xml \
+
 # Copy all custom init rc files
 $(foreach f,$(wildcard vendor/evolution/prebuilt/common/etc/init/*.rc),\
     $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))

Spark-Rom/platform_vendor_spark

config/common.mk
@@ -44,6 +44,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/spark/prebuilt/common/etc/init.local.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.spark.rc
 
+# Font service Permissions
+PRODUCT_COPY_FILES += \
+    vendor/spark/config/permissions/privapp-permissions-spark-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-spark-system_ext.xml \
+
 ifneq ($(AB_OTA_PARTITIONS),)
 PRODUCT_COPY_FILES += \
     vendor/spark/prebuilt/common/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \