1d721384a2363337aed4bff7338b1c3578045b20
spkal01 <kalligeross@gmail.com>
2021-12-05 12:13:59 +0100

    Improve Json Generation Script
    Change-Id: I46ce84ff7e99c5301e77c2b69b2b8c297fde4263

--- build/tools/generate_json.sh
+++ build/tools/generate_json.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-
-generate_json {
-
 if [ "$1" ]; then
     file_path=$1
     file_name=$(basename "$file_path")
+    echo "Generating .json"
     if [ -f $file_path ]; then
         device_name=$(echo $file_name | cut -d'-' -f5);
         build_prop="out/target/product/${device_name}/system/build.prop";
         name=$(grep ro\.spark\.device\.name ${build_prop} | cut -d= -f2);
         codename=$(grep ro\.spark\.device\= ${build_prop} | cut -d= -f2);
         version=$(grep ro\.spark\.version ${build_prop} | cut -d= -f2);
-        variant_lower=$(echo ${variant} | tr '[:upper:]' '[:lower:]');
         maintainer_name=$(grep ro\.spark\.maintainer\= ${build_prop} | cut -d= -f2);
         maintainer_username=$(grep ro\.spark\.maintainer\.username ${build_prop} | cut -d= -f2);
         size=$(stat -c%s $file_path);
@@ -29,8 +26,8 @@ if [ "$1" ]; then
         echo "  \"url\":\"${url}\"," >> $file_path.json
         echo "  \"group\":\"${group}\"" >> $file_path.json
         echo "}" >> $file_path.json
-        mv "${file_path}.json" "./${device_name}_${variant_lower}.json"
+        rm -rf "./${device_name}.json"
+        mv "${file_path}.json" "./${device_name}.json"
     fi
 fi
 
-}
