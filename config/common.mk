ecc2230a6bf6473d82a21fce29bbb5d86aa4d86b
spkal01 <kalligeross@gmail.com>
2021-12-14 20:11:25 +0100

    Revert "vendor: config: Set priv-app permission control to enforce"
    This reverts commit 39f4510d3a5b44af3cc65e5c7fe4db4dd8559b21.
    
    Change-Id: If894ae3496b5a77bf0143c6e60561a0e5ba555dd

--- config/common.mk
+++ config/common.mk
@@ -79,7 +79,7 @@ PRODUCT_COPY_FILES += \
 
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.control_privapp_permissions=enforce
+    ro.control_privapp_permissions=log
 
 # Fonts
 include vendor/spark/config/fonts.mk
