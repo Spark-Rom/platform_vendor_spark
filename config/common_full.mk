136d0e77d67aa2f2943bc348438d89f23374c9ea
DroidFreak32 <rushabshah32@gmail.com>
2023-05-06 14:07:56 +0530

    config: common-full: Build zstd cmdline tools
    * Installs the following:
    unzstd -> zstd
    zstd
    zstdcat -> zstd
    zstdgrep
    zstdless
    zstdmt -> zstd
    
    Change-Id: I3e1efae8028a3ac5f6b9d153c9e39eef1d51a34b

--- config/common_full.mk
+++ config/common_full.mk
@@ -3,6 +3,10 @@ $(call inherit-product, vendor/spark/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
+# Extra cmdline tools
+PRODUCT_PACKAGES += \
+    zstd
+
 # Recorder
 PRODUCT_PACKAGES += \
     Recorder
