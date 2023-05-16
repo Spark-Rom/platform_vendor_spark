thedoctorsz/android_vendor_rising

audio: Import sound effects pack
* taken from protonAOSP and rice A12

Co-authored-by: kdrag0n <danny@kdrag0n.dev>
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

audio/audio.mk
@@ -1,0 +1,61 @@
+# Copyright (C) 2022 Nothing Technology Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# NothingOS alarm ringtones
+# Source: https://www.getdroidtips.com/download-nothing-phone-1-ringtones-and-notification-tones/
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/alarm_ringtones/01_bedside.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/01_bedside.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/02_frogs.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/02_frogs.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/03_ramble.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/03_ramble.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/04_coil.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/04_coil.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/05_kashio.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/05_kashio.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/06_transmission.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/06_transmission.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/07_nothing.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/07_nothing.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/08_incoming.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/08_incoming.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/09_munge.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/09_munge.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/10_prong.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/10_prong.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/into_the_night.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/into_the_night.ogg
+    
+# NothingOS notification sounds
+# Source: https://www.getdroidtips.com/download-nothing-phone-1-ringtones-and-notification-tones/
+LOCAL_PATH := vendor/lineage/audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/notifications/01_oi!.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/01_oi!.ogg \
+    $(LOCAL_PATH)/notifications/02_bulb_one.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/02_bulb_one.ogg \
+    $(LOCAL_PATH)/notifications/03_bulb_two.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/03_bulb_two.ogg \
+    $(LOCAL_PATH)/notifications/04_guiro.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/04_guiro.ogg \
+    $(LOCAL_PATH)/notifications/05_volley.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/05_volley.ogg \
+    $(LOCAL_PATH)/notifications/06_squiggle.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/06_squiggle.ogg \
+    $(LOCAL_PATH)/notifications/07_isolator.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/07_isolator.ogg \
+    $(LOCAL_PATH)/notifications/08_gamma.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/08_gamma.ogg \
+    $(LOCAL_PATH)/notifications/09_beak.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/09_beak.ogg \
+    $(LOCAL_PATH)/notifications/10_nope.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/10_nope.ogg \
+    $(LOCAL_PATH)/notifications/oneplus.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/oneplus.ogg
+    
+# NothingOS ringtones
+# Source: https://www.getdroidtips.com/download-nothing-phone-1-ringtones-and-notification-tones/
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ringtones/01_pneumatic.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/01_pneumatic.ogg \
+    $(LOCAL_PATH)/ringtones/02_abra.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/02_abra.ogg \
+    $(LOCAL_PATH)/ringtones/03_plot.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/03_plot.ogg \
+    $(LOCAL_PATH)/ringtones/04_beetle.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/04_beetle.ogg \
+    $(LOCAL_PATH)/ringtones/05_squirrels.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/05_squirrels.ogg \
+    $(LOCAL_PATH)/ringtones/06_snaps.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/06_snaps.ogg \
+    $(LOCAL_PATH)/ringtones/07_radiate.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/07_radiate.ogg \
+    $(LOCAL_PATH)/ringtones/08_tennis.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/08_tennis.ogg \
+    $(LOCAL_PATH)/ringtones/09_coded.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/09_coded.ogg \
+    $(LOCAL_PATH)/ringtones/10_scribble.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/10_scribble.ogg \
+    $(LOCAL_PATH)/ringtones/into_the_night.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/into_the_night.ogg \
+    $(LOCAL_PATH)/ringtones/oneplusring.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/oneplusring.ogg
+

config/common.mk
@@ -1,6 +1,7 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/lineage/fonts/fonts.mk)
+$(call inherit-product-if-exists, vendor/lineage/audio/audio.mk)
 
 PRODUCT_BRAND ?= LineageOS
 

Spark-Rom/platform_vendor_spark

Change-Id: I6ae5c34d26e6aa90e881377fe7010624d69fe1eb

audio/audio.mk
@@ -1,0 +1,61 @@
+# Copyright (C) 2022 Nothing Technology Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# NothingOS alarm ringtones
+# Source: https://www.getdroidtips.com/download-nothing-phone-1-ringtones-and-notification-tones/
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/alarm_ringtones/01_bedside.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/01_bedside.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/02_frogs.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/02_frogs.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/03_ramble.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/03_ramble.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/04_coil.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/04_coil.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/05_kashio.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/05_kashio.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/06_transmission.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/06_transmission.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/07_nothing.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/07_nothing.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/08_incoming.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/08_incoming.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/09_munge.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/09_munge.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/10_prong.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/10_prong.ogg \
+    $(LOCAL_PATH)/alarm_ringtones/into_the_night.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/into_the_night.ogg
+
+# NothingOS notification sounds
+# Source: https://www.getdroidtips.com/download-nothing-phone-1-ringtones-and-notification-tones/
+LOCAL_PATH := vendor/spark/audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/notifications/01_oi!.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/01_oi!.ogg \
+    $(LOCAL_PATH)/notifications/02_bulb_one.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/02_bulb_one.ogg \
+    $(LOCAL_PATH)/notifications/03_bulb_two.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/03_bulb_two.ogg \
+    $(LOCAL_PATH)/notifications/04_guiro.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/04_guiro.ogg \
+    $(LOCAL_PATH)/notifications/05_volley.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/05_volley.ogg \
+    $(LOCAL_PATH)/notifications/06_squiggle.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/06_squiggle.ogg \
+    $(LOCAL_PATH)/notifications/07_isolator.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/07_isolator.ogg \
+    $(LOCAL_PATH)/notifications/08_gamma.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/08_gamma.ogg \
+    $(LOCAL_PATH)/notifications/09_beak.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/09_beak.ogg \
+    $(LOCAL_PATH)/notifications/10_nope.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/10_nope.ogg \
+    $(LOCAL_PATH)/notifications/oneplus.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/oneplus.ogg
+    
+# NothingOS ringtones
+# Source: https://www.getdroidtips.com/download-nothing-phone-1-ringtones-and-notification-tones/
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ringtones/01_pneumatic.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/01_pneumatic.ogg \
+    $(LOCAL_PATH)/ringtones/02_abra.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/02_abra.ogg \
+    $(LOCAL_PATH)/ringtones/03_plot.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/03_plot.ogg \
+    $(LOCAL_PATH)/ringtones/04_beetle.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/04_beetle.ogg \
+    $(LOCAL_PATH)/ringtones/05_squirrels.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/05_squirrels.ogg \
+    $(LOCAL_PATH)/ringtones/06_snaps.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/06_snaps.ogg \
+    $(LOCAL_PATH)/ringtones/07_radiate.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/07_radiate.ogg \
+    $(LOCAL_PATH)/ringtones/08_tennis.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/08_tennis.ogg \
+    $(LOCAL_PATH)/ringtones/09_coded.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/09_coded.ogg \
+    $(LOCAL_PATH)/ringtones/10_scribble.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/10_scribble.ogg \
+    $(LOCAL_PATH)/ringtones/into_the_night.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/into_the_night.ogg \
+    $(LOCAL_PATH)/ringtones/oneplusring.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/oneplusring.ogg
+

config/common.mk
@@ -2,6 +2,7 @@
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product, vendor/spark/config/bootanimation.mk)
 $(call inherit-product, vendor/addons/config.mk)
+$(call inherit-product-if-exists, vendor/spark/audio/audio.mk)
 
 PRODUCT_BRAND ?= Spark
 